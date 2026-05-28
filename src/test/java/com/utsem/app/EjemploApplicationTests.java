2f05f1f5e6d9c58a03e3280690a5550a8c7d82b2
Francisco Javier Gutierrez Herculano <chinopaco.05@gmail.com>
2026-05-27 19:06:25 -0600

    primer commit

---
+++ src/test/java/com/utsem/app/EjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utsem.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
